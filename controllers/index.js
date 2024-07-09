d9e95b58da07ebc8c6dabe313f77d5c0b5d920e6
gaillynnn <gaillynnneilson@gmail.com>
2024-07-08 21:29:45 -0500

    Added code to both api and controller files

---
+++ controllers/index.js
@@ -0,0 +1,44 @@
+const Item = require('../models/Item');
+
+// Controller for getting all items
+exports.getItems = async (req, res) => {
+  try {
+    const items = await Item.find();
+    res.json(items);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+};
+
+// Controller for adding a new item
+exports.addItem = async (req, res) => {
+  try {
+    const newItem = new Item({
+      name: req.body.name,
+    });
+
+    const item = await newItem.save();
+    res.json(item);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+};
+
+// Controller for deleting an item
+exports.deleteItem = async (req, res) => {
+  try {
+    const item = await Item.findById(req.params.id);
+
+    if (!item) {
+      return res.status(404).json({ msg: 'Item not found' });
+    }
+
+    await item.remove();
+    res.json({ msg: 'Item removed' });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+};
