8f009006c51a28a7dd856ee0357d3b0da6f1c609
gaillynnn <gaillynnneilson@gmail.com>
2024-07-15 18:23:30 -0500

    Fixed behavoir model

--- controllers/api/habitRoutes.js
+++ controllers/api/habitRoutes.js
@@ -14,6 +14,25 @@ router.get('/', (req, res) => {
         res.status(200).json(habits)
     })
 });
+router.get('/:id', (req, res)=> {
+    console.log({req, res});
+    // ModelName.findOne({
+        // Optional options
+        // Filtering results using where
+        // where: { firstColumn: "value" },
+        // Returning only specified columns
+        // attributes: ["firstColumn", "secondColumn"],
+    //   }).then((foundResult) => {});
+    Habit.findOne({
+        where: {
+        id: req.params.id
+        }
+    }).then((result) => {
+        console.log({result})
+    })
+
+    res.send("This route gets a single habit")
+})
 router.post('/', (req, res) => {
     console.log({req, res});
 });
