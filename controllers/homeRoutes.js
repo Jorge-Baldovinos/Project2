a1ce89d0d9ca254988cad8234e23070e42b5d07f
gaillynnn <gaillynnneilson@gmail.com>
2024-07-15 19:21:39 -0500

    Finished homeRoutes file

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,11 +1,21 @@
-const router = require('express').Router();
+// Import necessary models
+const Behavior = require('../models/Behavior');
+const User = require('../models/User');
+const Habit = require('../models/Habit');
 
+// Import controllers for business logic
+const { getBehaviors, createBehavior } = require('../controllers/behaviorController');
+const { getUsers, createUser } = require('../controllers/userController');
+const { getHabits, createHabit } = require('../controllers/habitController');
 
+// Define routes using the controllers
+router.get('/behaviors', getBehaviors);
+router.post('/behaviors', createBehavior);
 
+router.get('/users', getUsers);
+router.post('/users', createUser);
 
+router.get('/habits', getHabits);
+router.post('/habits', createHabit);
 
-
-router.get('/', async (req, res) => {
-    res.send('Hello World');
-});
 module.exports = router;
