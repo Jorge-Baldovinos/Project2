b60e58e66ff54e7c1b64d81d26728ff1e84cd7ac
gaillynnn <gaillynnneilson@gmail.com>
2024-07-15 20:57:19 -0500

    Added routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,21 +1,50 @@
-// Import necessary models
-const Behavior = require('../models/Behavior');
-const User = require('../models/User');
-const Habit = require('../models/Habit');
-
-// Import controllers for business logic
-const { getBehaviors, createBehavior } = require('../controllers/behaviorController');
-const { getUsers, createUser } = require('../controllers/userController');
-const { getHabits, createHabit } = require('../controllers/habitController');
-
-// Define routes using the controllers
-router.get('/behaviors', getBehaviors);
-router.post('/behaviors', createBehavior);
-
-router.get('/users', getUsers);
-router.post('/users', createUser);
-
-router.get('/habits', getHabits);
-router.post('/habits', createHabit);
-
-module.exports = router;
+const router = require('express').Router();
+router.get('/', async (req, res) => {
+    try {
+    
+  
+      res.render('profile');
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+  
+  
+  router.get('/login', async (req, res) => {
+      try {
+      
+    
+        res.render('login');
+      } catch (err) {
+        res.status(500).json(err);
+      }
+    });
+  
+//   router.get('/profile', withAuth, async (req, res) => {
+//     try {
+//       const userData = await User.findByPk(req.session.user_id, {
+//         attributes: { exclude: ['password'] },
+//         include: [{ model: Project }],
+//       });
+  
+//       const user = userData.get({ plain: true });
+  
+//       res.render('profile', {
+//         ...user,
+//         logged_in: true
+//       });
+//     } catch (err) {
+//       res.status(500).json(err);
+//     }
+//   });
+  
+//   router.get('/login', (req, res) => {
+//     if (req.session.logged_in) {
+//       res.redirect('/profile');
+//       return;
+//     }
+  
+//     res.render('login');
+//   });
+  
+  module.exports = router;
