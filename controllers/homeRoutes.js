de87494c11599be2ae4df189d045fd38287f67d7
Jbaldovinos <54425092+Jorge-Baldovinos@users.noreply.github.com>
2024-07-16 21:36:35 -0500

    refactored code

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const { User, Routine } = require('../models');
-const withAuth = require('../Utils/auth');
+const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
     try {
